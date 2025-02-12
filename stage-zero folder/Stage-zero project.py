ac66014f22020981d9ca82f17d0980246e00cc76
Judith Mbamalu <106772155+Clarejewel@users.noreply.github.com>
2025-02-12 04:45:40 +0000

    Update Stage-zero project.py

--- stage-zero folder/Stage-zero project.py
+++ stage-zero folder/Stage-zero project.py
@@ -70,3 +70,24 @@ print(Team_Proline1_Info["Member_4"]["Name"])
 
 print(Team_Proline1_Info["Member_4"]["Email"]) 
 #output: Judith Mbamalu
+
+
+#Organise the information in a more readable format
+
+print("Names                : ",Team_Proline1_Info ["Member_1"]["Name"],   Team_Proline1_Info ["Member_2"]["Name"],   Team_Proline1_Info ["Member_3"]["Name"],  Team_Proline1_Info ["Member_4"]["Name"],  Team_Proline1_Info ["Member_5"]["Name"])
+print("Slack Usernames      : ",Team_Proline1_Info ["Member_1"]["Slack Username"],  Team_Proline1_Info ["Member_2"]["Slack Username"],  Team_Proline1_Info ["Member_3"]["Slack Username"],  Team_Proline1_Info ["Member_4"]["Slack Username"],  Team_Proline1_Info ["Member_5"]["Slack Username"])
+print("Emails               : ",Team_Proline1_Info ["Member_1"]["Email"],  Team_Proline1_Info ["Member_2"]["Email"],  Team_Proline1_Info ["Member_3"]["Email"],  Team_Proline1_Info ["Member_4"]["Email"],  Team_Proline1_Info ["Member_5"]["Email"])
+print("Hobbies              : ",Team_Proline1_Info ["Member_1"]["Hobby"],  Team_Proline1_Info ["Member_2"]["Hobby"],  Team_Proline1_Info ["Member_3"]["Hobby"],  Team_Proline1_Info ["Member_4"]["Hobby"],  Team_Proline1_Info ["Member_5"]["Hobby"])
+print("Countries            : ",Team_Proline1_Info ["Member_1"]["Country"],  Team_Proline1_Info ["Member_2"]["Country"],  Team_Proline1_Info ["Member_3"]["Country"],  Team_Proline1_Info ["Member_4"]["Country"],  Team_Proline1_Info ["Member_5"]["Country"])
+print("Disciplines          : ",Team_Proline1_Info ["Member_1"]["Discipline"],  Team_Proline1_Info ["Member_2"]["Discipline"],  Team_Proline1_Info ["Member_3"]["Discipline"],  Team_Proline1_Info ["Member_4"]["Discipline"],  Team_Proline1_Info ["Member_5"]["Discipline"])
+print("Programming Languages: ", Team_Proline1_Info ["Member_1"]["Programming Language"],  Team_Proline1_Info ["Member_2"]["Programming Language"],  Team_Proline1_Info ["Member_3"]["Programming Language"],  Team_Proline1_Info ["Member_4"]["Programming Language"],  Team_Proline1_Info ["Member_5"]["Programming Language"])
+
+#Output: 
+#Names                :  Adeola Akintola Alamin Omitogun Chika Onyedikachukwu Judith Mbamalu Princewill_Ukot Eseoghene Cynthia
+#Slack Usernames      :  adeolaa Alamin Onyedikachukwu JudithM Ese
+#Emails               :  akintolaadeola09@gmail.com omitogunaminbabatunde@gmail.com chikadika18@gmail.com mbamalujudith@gmail.com ese.princewillukot@gmail.com
+#Hobbies              :  Surfing for information Running, reading Reading Travelling and Learning Reading and Coding
+#Countries            :  Nigeria Nigeria Nigeria Nigeria Nigeria
+#Disciplines          :  Public Health Pharmacology, Cell biology Medical Laboratory Science Plant Science Plant Science and Biotechnology
+#Programming Languages:  Python Python Python Python Python
+
